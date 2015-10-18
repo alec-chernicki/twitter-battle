7a2d83c63f4c727ea2ec56a4f1ba602bfdfa6449
Alec Ortega <alecortega@live.com>
2015-10-18 02:12:19 -0400

    Refactor with OOP concepts

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
+[![Code Climate](https://codeclimate.com/github/alecortega/twitter-battle/badges/gpa.svg)](https://codeclimate.com/github/alecortega/twitter-battle)
 # twitter-battle
 Compare how two search terms compete against each other in real-time.
 
 **Fully Responsive Demo: https://twitterbattle-alecortega.herokuapp.com/**
 
-**Goals:** 
+**Goals:**
 - To gain a better understanding on Socket.io + Express
-- Learn how to access the Twitter API and streaming API's in general 
+- Learn how to access the Twitter API and streaming API's in general
 - Learn how to use Redis as an easy key-value store.
 
 ![alt tag](http://i60.tinypic.com/5b9r9v.png)
--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var path = require('path');
-// TODO: MemoryStore isn't fit for prod, switch to Redis store, make sure compat w/ Socket.io
-var session = require('express-session')({
+    // TODO: MemoryStore isn't fit for prod, switch to Redis store, make sure compat w/ Socket.io
+var  session = require('express-session')({
   secret: process.env.EXPRESS_SESSION_SECRET,
   resave: false,
   saveUninitialized: true
@@ -12,6 +12,7 @@ var session = require('express-session')({
 // Creating new express app
 var express = require('express');
 var app = express();
+
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -51,23 +52,50 @@ var twit = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
+// Define Objects
+var User = function(id){
+  this.id = id;
+  this.searchCountOne = 0;
+  this.searchCountTwo = 0;
+  this.totalCount = 0;
+};
+
+User.prototype.addTweetOne = function(searchCount) {
+  this.searchCountOne++;
+  this.totalCount++;
+};
+
+User.prototype.addTweetTwo = function(searchCount) {
+  this.searchCountTwo++;
+  this.totalCount++;
+};
+
+var TweetData = function(tweet, count, totalCount) {
+  this.user = tweet.user.screen_name;
+  this.text = tweet.text;
+  this.userImage = tweet.user.profile_image_url;
+  this.searchCount =  count;
+  this.searchPercentage = (count / totalCount) * 100;
+};
+
+// Helper Methods
+var deleteUser = function(id){
+  redis.del(id, function(err, reply) {
+    if (err) console.log(err);
+  });
+};
+
 // Creates a new instance of Socket.io
 var ioSession = require('socket.io-express-session');
 var io = require('socket.io')(server);
 io.use(ioSession(session));
 
 io.on('connection', function(socket) {
-  var sid = socket.handshake.sessionID;
-  var searchOneCount = 0;
-  var searchTwoCount = 0;
-  var totalCount = 0;
   var stream;
+  var user = new User(socket.handshake.sessionID);
 
-  redis.lrange(sid, 0, 1, function(err, searchTerms) {
-    if (err || !searchTerms[0] || !searchTerms[1]) {
-      console.log('Error finding search terms in Redis');
-      return;
-    }
+  redis.lrange(user.id, 0, 1, function(err, searchTerms) {
+    if (err || !searchTerms[0] || !searchTerms[1]) return;
 
     // TODO: This is creating a new stream on every new connection which is very bad.
     // Once my JavaScript skillz level up come back and refactor this out into a global var.
@@ -77,26 +105,12 @@ io.on('connection', function(socket) {
       var text = tweet.text.toLowerCase();
 
       if (text.indexOf(' ' + searchTerms[0]) !== -1) {
-        searchOneCount++;
-        totalCount++;
-        socket.volatile.emit('tweet one', {
-          user: tweet.user.screen_name,
-          text: tweet.text,
-          userImage: tweet.user.profile_image_url,
-          searchCount: searchOneCount,
-          searchPercentage: (searchOneCount / totalCount) * 100
-        });
+        user.addTweetOne();
+        socket.volatile.emit('tweet one', new TweetData(tweet, user.searchCountOne, user.totalCount));
       }
       if (text.indexOf(' ' + searchTerms[1]) !== -1) {
-        searchTwoCount++;
-        totalCount++;
-        socket.volatile.emit('tweet two', {
-          user: tweet.user.screen_name,
-          text: tweet.text,
-          userImage: tweet.user.profile_image_url,
-          searchCount: searchTwoCount,
-          searchPercentage: (searchTwoCount / totalCount) * 100
-        });
+        user.addTweetTwo();
+        socket.volatile.emit('tweet two', new TweetData(tweet, user.searchCountTwo, user.totalCount));
       }
     });
   });
@@ -105,18 +119,14 @@ io.on('connection', function(socket) {
     if (stream) stream.stop();
     // Set timeout to account for page refresh
     setTimeout(function() {
-      redis.del(sid, function(err, reply) {
-        if (err) console.log(err);
-      });
+      deleteUser(user.id);
     }, 4000);
   });
 });
 
 // Create Express routes
 app.get('/', function(req, res) {
-  redis.del(req.sessionID, function(err, reply) {
-    if (err) console.log(err);
-  });
+  deleteUser(req.sessionID);
   res.render('landing');
 });
 
